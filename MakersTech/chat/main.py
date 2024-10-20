0aa7c097445f02d9b9713782cc74b30838c69269
ivan v <ivizcain4@cuc.edu.co>
2024-10-20 03:12:37 -0500

    feat(chatbot): Natural responce

--- MakersTech/chat/main.py
+++ MakersTech/chat/main.py
@@ -1,14 +1,74 @@
 import ollama
+from MakersTech.MakersTechApp import views
 
-def call_chat(prompt: str):
+MODEL = 'llama3.2'
+
+FUNCTIONS = {}
+
+TOOLS = [
+    {
+    'type': 'function',
+    'function': {
+        'name': '',
+        'description': '',
+        'parameters': {
+            'type': 'object',
+            'properties': {
+            'number': {
+                'type': '',
+                'description': '',
+                },
+            },
+            'required': [''],
+            },
+        },
+    },
+    ]
+
+def call_chat(prompt: str, tools=None):
     try:
-        formatted_prompt = prompt + 'extremly short responce'
+        messages = [{'role': 'user', 'content': prompt}]
+        if tools:
+            response = ollama.chat(
+                model=MODEL,
+                messages=messages,
+                tools=tools)
+        else:
+            response = ollama.chat(
+                model=MODEL,
+                messages=messages)
+        
+        if 'tool_calls' in response['message']:
+            for tool_call in response['message']['tool_calls']:
+                function_name = tool_call['function']['name']
+                arguments = tool_call['function']['arguments']
+                
+                # Verificar si la función existe en el registro
+                if function_name in FUNCTIONS:
+                    # Llamar a la función y obtener el resultado
+                    result = FUNCTIONS[function_name](**arguments)
+                    return response, result
 
-        response = ollama.chat(model='llama3.2',
-                               messages=[{'role': 'user',
-                                          'content': formatted_prompt}])
+        return response, None
 
-        return response
     except Exception as e:
         print(f"Error al llamar al chatbot: {e}")
+        return None, None
+
+def start_chat(prompt: str):
+    try:
+        response, results = call_chat(
+            prompt=prompt,
+            tools=TOOLS
+        )
+        output_prompt = (
+            f"Context: Your goal is to provide a friendly and informative response to the user query: "
+            f'"{prompt}". Use the following information to construct your response: '
+            f'{results if results else "No result from function."} '
+            "Ensure the response is engaging and guides the user towards making a purchase decision. (short responce)"
+            )
+        return call_chat(prompt=output_prompt, tools=None)
+
+    except Exception as e:
+        print(f"Error al iniciar chat: {e}")
         return None
